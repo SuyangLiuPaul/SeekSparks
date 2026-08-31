79e953396a93fa824e8561b1c638cff2ae7cbd18
Paul Liu <pliu0036@mu00188089x.tail4809e4.ts.net>
2026-09-01 05:48:11 +1000

    chore: release v1.6.197 (dev)
    Jacob's journey (#317) deployed to dev.

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -2215,7 +2215,7 @@ const String kAppVersion = _envAppVersion == '' ? '1.3.113' : _envAppVersion;
 /// for the build, which in practice means dev workflow only.
 const String kAppReleaseTime = String.fromEnvironment(
   'APP_RELEASE_TIME',
-  defaultValue: '2026-08-31T15:31:40Z',
+  defaultValue: '2026-08-31T19:43:14Z',
 );
 
 /// Returns a user-locale-formatted release time string. Parses
