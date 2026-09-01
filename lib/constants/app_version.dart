45b29d1d1916adcc2d301c248f37bd5c9079b2c2
SeekSparks Loop <lsy95112@gmail.com>
2026-09-02 00:03:37 +1000

    release: v1.6.200 (dev)

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -2215,7 +2215,7 @@ const String kAppVersion = _envAppVersion == '' ? '1.3.113' : _envAppVersion;
 /// for the build, which in practice means dev workflow only.
 const String kAppReleaseTime = String.fromEnvironment(
   'APP_RELEASE_TIME',
-  defaultValue: '2026-09-01T08:37:01Z',
+  defaultValue: '2026-09-01T13:59:29Z',
 );
 
 /// Returns a user-locale-formatted release time string. Parses
