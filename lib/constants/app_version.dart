678bb4d2a2a1627b652e88211a6bf57e3a44ace6
Paul Liu <pliu0036@mu00188089x.tail4809e4.ts.net>
2026-09-02 17:00:43 +1000

    release: v1.6.205 (dev+prod)

--- lib/constants/app_version.dart
+++ lib/constants/app_version.dart
@@ -2215,7 +2215,7 @@ const String kAppVersion = _envAppVersion == '' ? '1.3.113' : _envAppVersion;
 /// for the build, which in practice means dev workflow only.
 const String kAppReleaseTime = String.fromEnvironment(
   'APP_RELEASE_TIME',
-  defaultValue: '2026-09-02T02:34:00Z',
+  defaultValue: '2026-09-02T06:58:46Z',
 );
 
 /// Returns a user-locale-formatted release time string. Parses
